f3b64736a54d3ca7a6ec12e14c4c519e32cbb90d
Khizar Khan <khizer.khan98@gmail.com>
2020-12-09 16:08:20 +0500

    Initial commit

---
+++ README.txt
@@ -0,0 +1,12 @@
+
+Welcome to SECRET_K Language Generator
+
+Use this cypher generator to encode normal texts into
+SECRET_K for secure communications. 
+
+SECRET_K is NSA's cypher algorithm used for communication
+with E-T civilizations. Use with care.
+
+Regards,
+Dr.Dwight Kurt Schrute
+Former Chief Engineer Inter Galactic Peace Corp.
---
+++ secret_k.py
@@ -0,0 +1,37 @@
+
+import time, sys, os
+from pygame import mixer
+
+mixer.init()
+mixer.music.load(str(os.getcwd())+str("/Pay Phone.mp3"))
+mixer.music.play()
+
+menu_resp = 0
+
+while menu_resp != 3:
+    print("\n1 => Encode\n2 => Decode\n3 => Exit")
+    menu_resp = int(input("\nSelect and option => "))
+
+    if menu_resp == 1:
+        cypher_string = str("")
+        plain_text = str(input("\nType message: "))
+
+        for i in range(len(plain_text)):
+            cypher_string = cypher_string + chr(ord(plain_text[i])+1)
+        
+        print("\nSecret_K message: ",cypher_string)
+
+    elif menu_resp == 2:
+        cypher_string = str(input("\nSecret_K message: "))
+        plain_text = str("")
+
+        for i in range(len(cypher_string)):
+            plain_text = plain_text + chr(ord(cypher_string[i])-1)
+
+        print("\nOriginal message: ",plain_text)
+
+    elif menu_resp == 3:
+        print("\nExiting. Food for thought, what if Area51 is just a cover and the real things happen at Area52?")
+        time.sleep(7)
+        sys.exit()
+        
